71ec3559bb4553c26e603d84dcbbac0c403ff5dc
technophoenix1020 <technophoenix1020@gmail.com>
2024-05-03 17:45:44 +0000

    Added Hello World and Values

---
+++ Values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* Adding two strings together */
+	fmt.Println("go" + "lang")
+
+	/* Math */
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	/* Boolean statements */
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
